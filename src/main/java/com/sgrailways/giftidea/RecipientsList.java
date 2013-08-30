5614b9335ae3366ead17ea2d912802d632470d64
sghill <sghill.dev@gmail.com>
2013-08-29 22:01:21 -0400

    made highlighting green

--- src/main/java/com/sgrailways/giftidea/RecipientsList.java
+++ src/main/java/com/sgrailways/giftidea/RecipientsList.java
@@ -1,6 +1,5 @@
 package com.sgrailways.giftidea;
 
-import android.R;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -9,8 +8,6 @@ import android.support.v4.widget.SimpleCursorAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListAdapter;
-import android.widget.ListView;
 import android.widget.TextView;
 import com.google.inject.Inject;
 import roboguice.fragment.RoboListFragment;
@@ -25,13 +22,30 @@ public class RecipientsList extends RoboListFragment {
     @InjectResource(com.sgrailways.giftidea.R.string.no_recipients_message) String noRecipientsMessage;
 
     @Override public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        ListAdapter adapter = new SimpleCursorAdapter(
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(
                 this.getActivity(),
-                R.layout.simple_list_item_1,
+                R.layout.recipient_item,
                 cursor(),
                 new String[]{NAME},
-                new int[]{R.id.text1}
+                new int[]{R.id.name}
         );
+        adapter.setViewBinder(new SimpleCursorAdapter.ViewBinder() {
+            public boolean setViewValue(View view, Cursor cursor, int columnIndex) {
+                TextView name = (TextView) view.getRootView().findViewById(R.id.name);
+                if (name == null) {
+                    return false;
+                }
+                name.setText(cursor.getString(1));
+                name.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        Intent intent = new Intent(RecipientsList.this.getActivity(), RecipientIdeasActivity.class);
+                        intent.putExtra("recipient", ((TextView) v.findViewById(R.id.name)).getText().toString());
+                        startActivity(intent);
+                    }
+                });
+                return true;
+            }
+        });
         setListAdapter(adapter);
         return super.onCreateView(inflater, container, savedInstanceState);
     }
@@ -41,12 +55,6 @@ public class RecipientsList extends RoboListFragment {
         setEmptyText(noRecipientsMessage);
         getActivity().setTitle(com.sgrailways.giftidea.R.string.gift_recipients_title);
         super.onResume();
-    }
-
-    @Override public void onListItemClick(ListView l, View v, int position, long id) {
-        Intent intent = new Intent(this.getActivity(), RecipientIdeasActivity.class);
-        intent.putExtra("recipient", ((TextView) v).getText().toString());
-        startActivity(intent);
     }
 
     private Cursor cursor() {
