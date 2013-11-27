c8eca24f60107b273e660c1f4c8ebb4e9069587e
sghill <sghill@sgrailways.com>
2013-11-27 01:06:10 -0500

    change AboutActivity to a PreferenceFragment

--- src/main/java/com/sgrailways/giftidea/AboutActivity.java
+++ src/main/java/com/sgrailways/giftidea/AboutActivity.java
@@ -1,35 +1,23 @@
 package com.sgrailways.giftidea;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.text.method.LinkMovementMethod;
-import android.view.View;
-import android.widget.TextView;
-import roboguice.activity.RoboActivity;
-import roboguice.inject.InjectView;
 
-public class AboutActivity extends RoboActivity {
-    @InjectView(R.id.contact_blog) TextView blog;
-    @InjectView(R.id.contact_twitter) TextView twitter;
-    @InjectView(R.id.contact_email) TextView email;
+import roboguice.activity.RoboPreferenceActivity;
 
+public class AboutActivity extends RoboPreferenceActivity {
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about);
         setTitle(R.string.about_title);
+        getFragmentManager()
+                .beginTransaction()
+                .replace(android.R.id.content, new PreferenceFragment())
+                .commit();
     }
 
-    @Override protected void onResume() {
-        blog.setMovementMethod(LinkMovementMethod.getInstance());
-        twitter.setMovementMethod(LinkMovementMethod.getInstance());
-        email.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent sendIntent = new Intent(Intent.ACTION_SENDTO);
-                sendIntent.setData(Uri.parse("mailto:sghill@sgrailways.com?subject=" + Uri.encode("GiftIdea! Feedback")));
-                startActivity(Intent.createChooser(sendIntent, "Send email"));
-            }
-        });
-        super.onResume();
+    class PreferenceFragment extends android.preference.PreferenceFragment {
+        @Override public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.preferences);
+        }
     }
 }
