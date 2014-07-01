e1d8fe2ba69140761cdbf8e200b37b4c52b260a8
Steve Hill <sghill@sgrailways.com>
2014-06-30 17:56:00 -0700

    added pmd with basic and braces rulesets

--- core/src/main/java/com/sgrailways/giftidea/core/domain/Idea.java
+++ core/src/main/java/com/sgrailways/giftidea/core/domain/Idea.java
@@ -39,16 +39,13 @@ public class Idea {
     }
 
     @Override public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
+        if (this == o) { return true; }
+        if (o == null || getClass() != o.getClass()) { return false; }
         Idea idea = (Idea) o;
-
-        if (done != idea.done) return false;
-        if (id != idea.id) return false;
-        if (recipientId != idea.recipientId) return false;
-        if (text != null ? !text.equals(idea.text) : idea.text != null) return false;
-
+        if (done != idea.done) { return false; }
+        if (id != idea.id) { return false; }
+        if (recipientId != idea.recipientId) { return false; }
+        if (text != null ? !text.equals(idea.text) : idea.text != null) { return false; }
         return true;
     }
 
